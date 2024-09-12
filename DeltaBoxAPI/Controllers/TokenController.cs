47dbdfa95c38a8daf2525f4e337579f87e890531
ShantoEC73 <ShantoEC73@SHANTOEC73>
2024-09-12 17:57:04 +0600

    Create token controller

---
+++ DeltaBoxAPI/Controllers/TokenController.cs
@@ -0,0 +1,20 @@
+﻿using DeltaBoxAPI.Application.Common.Models.Repository;
+using DeltaBoxAPI.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeltaboxAPI.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ITokenRepository _tokenRepository;
+
+        public TokenController(ApplicationDbContext context, ITokenRepository tokenRepository)
+        {
+            _context = context;
+            _tokenRepository = tokenRepository;
+        }
+    }
+}
