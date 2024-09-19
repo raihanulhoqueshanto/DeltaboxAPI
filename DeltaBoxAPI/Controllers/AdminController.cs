e9534ec5785336995aca4b3595a46c4ff5b72d4f
ShantoEC73 <ShantoEC73@SHANTOEC73>
2024-09-19 13:53:09 +0600

    Added admin and protected controller for test purposes

---
+++ DeltaBoxAPI/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeltaboxAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            return Ok("Data fron admin controller");
+        }
+    }
+}
